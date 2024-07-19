1949e828a67ada8d5eadece9d1375ac323633314
Prachi Jamdade <prachiii@Prachis-Laptop.local>
2024-07-19 23:37:13 +0530

    chore: change .sh

--- action/git_update.sh
+++ action/git_update.sh
@@ -5,9 +5,9 @@ VERSION=""
 # get parameters
 while getopts v: flag
 do
-    case "${flag}" in
-        v) VERSION=${OPTARG};;
-    esac
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
 done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
@@ -15,30 +15,31 @@ git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
-then CURRENT_VERSION='v0.1.0'
+then
+  CURRENT_VERSION='v0.1.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
-#replace . with space so we can split into an array
-CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./})
+# replace . with space so can split into an array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == "major" ]]
+if [[ $VERSION == 'major' ]]
 then
-    VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == "minor" ]]
-then 
-    VNUM2=v$((VNUM2+1))
-elif [[ $VERSION == "patch" ]]
-then 
-    VNUM3=v$((VNUM3+1))
-else 
-    echo "No Version Type or incorrect type specified, try: -v [major, minor, patch]"
-    exit 1
+  VNUM1=v$((VNUM1+1))
+elif [[ $VERSION == 'minor' ]]
+then
+  VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'patch' ]]
+then
+  VNUM3=$((VNUM3+1))
+else
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
 fi
 
 # create new tag
@@ -51,12 +52,12 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
-    echo "Tagged with $NEW_TAG"
-    git tag $NEW_TAG
-    git push --tags
-    git push
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
 else
-    echo "Already a tag on this commit"
+  echo "Already a tag on this commit"
 fi
 
 echo ::set-output name=new-version::$NEW_TAG
